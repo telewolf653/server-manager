2d9e58fe6be3e821fb657a62c04c5c99f1e93b15
ashiwolf <telewolf653@gmail.com>
2016-09-02 17:38:01 +0430

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -5,7 +5,7 @@ URL = require('socket.url')
 JSON = require('dkjson')
 HTTPS = require('ssl.https')
 ----config----
-local bot_api_key = "225937844:AAHG3qQBfce6PazsBXUboBMfgIERjn5eLSg" --pit your api key of api telegram bot
+local bot_api_key = "توکن رباتو اینجا بزارین" --pit your api key of api telegram bot
 local BASE_URL = "https://api.telegram.org/bot"..bot_api_key
 -- Base folder like
 -- local BASE_FOLDER = "/home/imandaneshi/files/"
@@ -16,7 +16,7 @@ local BASE_FOLDER = ""
 
 function is_admin(msg)-- Check if user is admin or not
   local var = false
-  local admins = {199471668,249002400}-- put your id and cli pika launcher bot
+  local admins = {اینجا ایدی عددی ربات لانچر و خودتون رو}-- put your id and cli pika launcher bot
   for k,v in pairs(admins) do
     if msg.from.id == v then
       var = true
